53a0071ea64bed82ea75a3a6c83f89bb523ed814
Rannojit <cserannojitsarkar22@gmail.com>
2020-05-03 00:40:33 +0600

    home page inita desktop view done

--- themes/dmwatch/assets/js/main.js
+++ themes/dmwatch/assets/js/main.js
@@ -147,6 +147,161 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 }
 
+/*Shoriful*/
+
+
+
+
+
+
+/*Rannojit*/
+
+if( $('.mainSlider').length ){
+    $('.mainSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+}
+
+
+if( $('.hmPracticeAreaSlider').length ){
+    $('.hmPracticeAreaSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+if( $('.hmFeaServicesSlider').length ){
+    $('.hmFeaServicesSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+if( $('.hmProTabSlider').length ){
+    $('.hmProTabSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+if( $('.fl-tabs').length ){
+
+  $('div.fl-tabs button').click(function(){
+    $('.hmProTabSlider').slick('refresh');
+      var tab_id = $(this).attr('data-tab');
+
+      $('div.fl-tabs button').removeClass('current');
+      $('.fl-tab-content').removeClass('current');
+
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+  });
+}
+
     new WOW().init();
 
 })(jQuery);
