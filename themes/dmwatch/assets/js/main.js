ce7355ae49b6c961bbabb57a2ea4d736ec4b8625
Rannojit <cserannojitsarkar22@gmail.com>
2020-05-03 21:43:54 +0600

    home page dektop view fix done

--- themes/dmwatch/assets/js/main.js
+++ themes/dmwatch/assets/js/main.js
@@ -158,12 +158,16 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 if( $('.mainSlider').length ){
     $('.mainSlider').slick({
+      pauseOnHover: false,
+      autoplay: true,
+      autoplaySpeed: 5000,
       dots: true,
       infinite: false,
-      arrows: false,
-      speed: 300,
+      arrows:false,
+      speed: 700,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      fade: true
     });
 }
 
@@ -255,6 +259,45 @@ if( $('.hmProTabSlider').length ){
       arrows: false,
       speed: 300,
       slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+if( $('.clientsPartnersLogosSlider').length ){
+    $('.clientsPartnersLogosSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 5,
       slidesToScroll: 1,
       responsive: [
         {
