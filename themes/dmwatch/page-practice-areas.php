85c31a4b7eb139339559dd59f5627e4a4779fd98
azizulsoftsw <azizul.softsw@gmail.com>
2020-05-23 20:23:39 +0600

    AZ

--- themes/dmwatch/page-practice-areas.php
+++ themes/dmwatch/page-practice-areas.php
@@ -133,25 +133,90 @@ $intro = get_field('introsec', $thisID);
           <?php if( $query->have_posts() ):?>
           <ul class="reset-list clearfix">
             <?php 
+              $i = 1;
               while($query->have_posts()): $query->the_post();
-              $attach_id = get_post_thumbnail_id(get_the_ID());
+               $fullname = get_field('full_name', get_the_ID()); 
+               $position = get_field('position', get_the_ID()); 
+               $profileimage = get_field('profile_image', get_the_ID()); 
+               $aboutcont = get_field('aboutcont', get_the_ID());
             ?>
             <li>
               <div class="dm-pa-grid-innr">
                 <div class="dm-pa-grid-img-cntlr">
                   <div class="dm-pa-grid-img">
+                    <?php if(!empty( $profileimage )): ?>
+                      <?php echo cbv_get_image_tag( $profileimage ); ?>
+                    <?php else: ?>
                     <img src="<?php echo THEME_URI; ?>/assets/images/dm-pa-grid-img.png">
+                    <?php endif; ?>
                     <div class="dm-pa-grid-img-hover"></div>
-                    <a href="#" class="overlay-link"></a>
+                    <a id="quickViewOpener" data-toggle="modal" data-target="#quickViewModal<?php echo $i; ?>" href="#" class="popup-btn"></a>
                   </div>
                 </div>
                 <div class="dm-pa-grid-dsc">
-                  <h4 class="dm-pa-grid-dsc-title">PERSON NAME</h4>
-                  <span>DESIGNATION</span>
+                  <h4 class="dm-pa-grid-dsc-title"><?php the_title( );?></h4>
+                  <?php if( !empty($position) ) printf('<span>%s</span>', $position); ?>
+                </div>
+              </div>
+              <!-- Modal -->
+              <div class="modal fade dm-modal-con-wrap" id="quickViewModal<?php echo $i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <button type="button" class="close popup-close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true"><img src="<?php echo THEME_URI; ?>/assets/images/close-icon.jpg"></span>
+                    </button>
+                    <div class="info-popup-container">
+                      <div class="info-popup-cntlr-inr clearfix">
+                        <div class="info-popup-sidebar">
+                          <div class="crkmts-grd-item">
+                            <div class="itm-grd-img-bx">
+                              <?php if(!empty( $profileimage )): echo cbv_get_image_tag( $profileimage ); endif;?>
+                            </div>
+                            <div class="itm-short-des">
+                              <strong>
+                                <?php 
+                                  if( !empty($fullname) ) 
+                                    printf('%s', $fullname);
+                                  else
+                                    printf('%s', get_the_title(get_the_ID()));
+                                ?>
+                              </strong>
+                              <?php if( !empty($position) ) printf('<span>%s</span>', $position); ?>
+                            </div>
+                          </div>
+                          <?php $sinfo = get_field('socialinfo', get_the_ID()); ?>
+                          <div class="popup-social">
+                            <label>Social Media</label>
+                            <?php if( !empty($sinfo) ): ?>
+                            <ul class="reset-list">
+                              <?php if( !empty($sinfo['linkedin_url']) ): ?>
+                              <li><a href="<?php echo $sinfo['linkedin_url']; ?>"><img src="<?php echo THEME_URI; ?>/assets/images/instagram.png"></a></li>
+                              <?php endif; ?>
+                              <?php if( !empty($sinfo['facebook_url']) ): ?>
+                              <li><a href="<?php echo $sinfo['facebook_url']; ?>"><img src="<?php echo THEME_URI; ?>/assets/images/facebook.png"></a></li>
+                              <?php endif; ?>
+                              <?php if( !empty($sinfo['twitter_url']) ): ?>
+                              <li><a href="<?php echo $sinfo['twitter_url']; ?>"><img src="<?php echo THEME_URI; ?>/assets/images/twitter.png"></a></li>
+                              <?php endif; ?>
+                              <?php if( !empty($sinfo['instagram_url']) ): ?>
+                              <li><a href="<?php echo $sinfo['instagram_url']; ?>"><img src="<?php echo THEME_URI; ?>/assets/images/linkedin.png"></a></li>
+                              <?php endif; ?>
+                            </ul>
+                            <?php endif; ?>
+                          </div>
+                        </div>
+                        <div class="info-popup-des">
+                          <?php if( !empty($aboutcont) ) echo wpautop( $aboutcont ); ?>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  </div>
                 </div>
               </div>
             </li>
-          <?php endwhile; ?>
+          <?php $i++; endwhile; ?>
           </ul>
           <?php endif; wp_reset_postdata(); ?>
         </div>
